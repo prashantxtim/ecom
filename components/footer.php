df9ffa5440a8b3069858022bf9127ad13a087cd2
Ashok GC <78782373+Ashok-gc@users.noreply.github.com>
2023-07-08 01:14:33 +0545

    first

---
+++ components/footer.php
@@ -0,0 +1,27 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <img src="images/logo-3.png" alt="">
+         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam quia atque soluta exercitationem rerum ab neque eum, rem accusantium nam id nemo fuga quod. Perferendis error quia explicabo optio? Ea?</p>
+      </div>
+
+      <div class="box">
+         <h3>contact us</h3>
+         <a href="tel:1234567890"><i class="fas fa-phone"></i> +977 9860 914 588</a>
+         <a href="mailto:shaikh@gmail.com"><i class="fas fa-envelope"></i> contact@gmail.com</a>
+         <a href="https://www.google.com/myplace"><i class="fas fa-map-marker-alt"></i> Koteshor </a>
+      </div>
+
+      <div class="box">
+         <h3>Quick links</h3>
+         <a href="shop.php"> <i class="fas fa-angle-right"></i> All Products</a>
+         <a href="login.php"> <i class="fas fa-angle-right"></i> Signin</a>
+         <a href="user_register.php"> <i class="fas fa-angle-right"></i> Sign Up</a>
+      </div>
+
+
+
+   </section>
+</footer>
---
+++ home.php
@@ -0,0 +1,209 @@
+<?php
+
+include 'components/connect.php';
+
+session_start();
+
+if(isset($_SESSION['user_id'])){
+   $user_id = $_SESSION['user_id'];
+}else{
+   $user_id = '';
+};
+
+include 'components/wishlist_cart.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>home</title>
+
+   <link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css" />
+   
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<?php include 'components/user_header.php'; ?>
+
+<div class="home-bg">
+
+<section class="home">
+
+   <div class="swiper home-slider">
+   
+   <div class="swiper-wrapper">
+
+      <div class="swiper-slide slide">
+         <div class="content">
+            <span>The Biggest Marketplace for</span>
+            <h3>Digital Prodicts</h3>
+            <p>Get the best deals on electronics, laptops, mobiles, watches, TVs, accessories, etc.</p>
+            <a href="shop.php" class="btn">All Items</a>
+         </div>
+         <div class="image">
+            <img src="images/3.png" alt="">
+         </div>
+      </div>
+   </div>
+
+</section>
+
+</div>
+
+<section class="home-products">
+
+   <h1 class="heading">Featured products</h1>
+
+   <div class="swiper products-slider">
+
+   <div class="swiper-wrapper">
+
+   <?php
+     $select_products = $conn->prepare("SELECT * FROM `products` LIMIT 6"); 
+     $select_products->execute();
+     if($select_products->rowCount() > 0){
+      while($fetch_product = $select_products->fetch(PDO::FETCH_ASSOC)){
+   ?>
+   <form action="" method="post" class="swiper-slide slide">
+      <input type="hidden" name="pid" value="<?= $fetch_product['id']; ?>">
+      <input type="hidden" name="name" value="<?= $fetch_product['name']; ?>">
+      <input type="hidden" name="price" value="<?= $fetch_product['price']; ?>">
+      <input type="hidden" name="image" value="<?= $fetch_product['image_01']; ?>">
+      <button class="fas fa-heart" type="submit" name="add_to_wishlist"></button>
+      <a href="quick_view.php?pid=<?= $fetch_product['id']; ?>" class="fas fa-eye"></a>
+      <img src="uploaded_img/<?= $fetch_product['image_01']; ?>" alt="">
+      <div class="name"><?= $fetch_product['name']; ?></div>
+      <div class="flex">
+         <div class="price"><span>$</span><?= $fetch_product['price']; ?><span>/-</span></div>
+         <input type="number" name="qty" class="qty" min="1" max="99" onkeypress="if(this.value.length == 2) return false;" value="1">
+      </div>
+      <input type="submit" value="add to cart" class="btn" name="add_to_cart">
+   </form>
+   
+   <?php
+      }
+   }else{
+      echo '<p class="empty">no products added yet!</p>';
+   }
+   ?>
+
+   </div>
+
+   <div class="swiper-pagination"></div>
+
+   </div>
+
+
+</section>
+
+<div class="home-bg">
+<section class="about-section">
+  <div class="about-topic">
+    <h1>Digi Presets Features</h1>
+  </div>
+  <div class="about-description">
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo, ea iste perspiciatis eos cum vero sint deleniti incidunt ut repellendus quibusdam, vel nobis quos error nostrum officiis, aperiam id facere.</p>
+  </div>
+  <div class="box-container">
+    <div class="box">
+      <div class="circle">
+        <img src="images/1.png" alt="Image">
+      </div>
+      <h2>Quality Platform</h2>
+      <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto, placeat, laborum architecto sequi repellat quaerat temporibus corrupti nihil laudantium quia quasi rerum ratione, suscipit nemo esse necessitatibus? Beatae, eos aliquid.</span>
+    </div>
+    <div class="box">
+      <div class="circle">
+        <img src="images/2.png" alt="Image">
+      </div>
+      <h2>Premium User</h2>
+      <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto, placeat, laborum architecto sequi repellat quaerat temporibus corrupti nihil laudantium quia quasi rerum ratione, suscipit nemo esse necessitatibus? Beatae, eos aliquid.</span>
+    </div>
+    <div class="box">
+      <div class="circle">
+        <img src="images/33.png" alt="Image">
+      </div>
+      <h2>Cloud Upload</h2>
+      <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto, placeat, laborum architecto sequi repellat quaerat temporibus corrupti nihil laudantium quia quasi rerum ratione, suscipit nemo esse necessitatibus? Beatae, eos aliquid.</span>
+    </div>
+  </div>
+</section>
+
+
+</div>
+
+
+<?php include 'components/footer.php'; ?>
+
+<script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
+
+<script src="js/script.js"></script>
+
+<script>
+
+var swiper = new Swiper(".home-slider", {
+   loop:true,
+   spaceBetween: 20,
+   pagination: {
+      el: ".swiper-pagination",
+      clickable:true,
+    },
+});
+
+ var swiper = new Swiper(".category-slider", {
+   loop:true,
+   spaceBetween: 20,
+   pagination: {
+      el: ".swiper-pagination",
+      clickable:true,
+   },
+   breakpoints: {
+      0: {
+         slidesPerView: 2,
+       },
+      650: {
+        slidesPerView: 3,
+      },
+      768: {
+        slidesPerView: 4,
+      },
+      1024: {
+        slidesPerView: 5,
+      },
+   },
+});
+
+var swiper = new Swiper(".products-slider", {
+   loop:true,
+   spaceBetween: 20,
+   pagination: {
+      el: ".swiper-pagination",
+      clickable:true,
+   },
+   breakpoints: {
+      550: {
+        slidesPerView: 2,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+   },
+});
+
+</script>
+
+</body>
+</html>
