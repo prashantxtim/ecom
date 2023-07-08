728f880ff2a424f3668f8477fe096807b5aa0aa2
Ashok GC <78782373+Ashok-gc@users.noreply.github.com>
2023-07-08 14:25:23 +0545

    done

--- checkout.php
+++ checkout.php
@@ -4,15 +4,16 @@ include 'components/connect.php';
 
 session_start();
 
-if(isset($_SESSION['user_id'])){
+if (isset($_SESSION['user_id'])) {
    $user_id = $_SESSION['user_id'];
-}else{
+} else {
    $user_id = '';
    header('location:user_login.php');
-};
+}
 
-if(isset($_POST['order'])){
+$message = [];
 
+if (isset($_POST['order'])) {
    $name = $_POST['name'];
    $name = filter_var($name, FILTER_SANITIZE_STRING);
    $number = $_POST['number'];
@@ -21,159 +22,132 @@ if(isset($_POST['order'])){
    $email = filter_var($email, FILTER_SANITIZE_STRING);
    $method = $_POST['method'];
    $method = filter_var($method, FILTER_SANITIZE_STRING);
-   $address = 'flat no. '. $_POST['flat'] .', '. $_POST['street'] .', '. $_POST['city'] .', '. $_POST['state'] .', '. $_POST['country'] .' - '. $_POST['pin_code'];
-   $address = filter_var($address, FILTER_SANITIZE_STRING);
+   $address = ''; // Empty address string
    $total_products = $_POST['total_products'];
    $total_price = $_POST['total_price'];
    $link = $_POST['link'];
 
-
    $check_cart = $conn->prepare("SELECT * FROM `cart` WHERE user_id = ?");
    $check_cart->execute([$user_id]);
 
-   if($check_cart->rowCount() > 0){
-
+   if ($check_cart->rowCount() > 0) {
       $insert_order = $conn->prepare("INSERT INTO `orders` (user_id, name, number, email, method, address, total_products, total_price, link) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
       $insert_order->execute([$user_id, $name, $number, $email, $method, $address, $total_products, $total_price, $link]);
-
 
       $delete_cart = $conn->prepare("DELETE FROM `cart` WHERE user_id = ?");
       $delete_cart->execute([$user_id]);
 
-      $message[] = 'order placed successfully!';
+      $message[] = 'Order placed successfully!';
       header('Location: thank_you.php');
-   }else{
-      $message[] = 'your cart is empty';
+      exit;
+   } else {
+      $message[] = 'Your cart is empty';
    }
-
 }
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>checkout</title>
-   
-   <!-- font awesome cdn link  -->
+   <title>Checkout</title>
+
+   <!-- Font Awesome CDN link -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
 
-   <!-- custom css file link  -->
+   <!-- Custom CSS file link -->
    <link rel="stylesheet" href="css/style.css">
 
 </head>
+
 <body>
-   
-<?php include 'components/user_header.php'; ?>
-<div class="topheader2">
-   <h3>Checkout</h3>
-   <p><a href="home.php">Home</a> <span> > Checkout</span></p>
-</div>
-<section class="checkout-orders">
 
-   <form action="" method="POST">
+   <?php include 'components/user_header.php'; ?>
+   <div class="topheader2">
+      <h3>Checkout</h3>
+      <p><a href="home.php">Home</a> <span> > Checkout</span></p>
+   </div>
+   <section class="checkout-orders">
 
-   <h3>your orders</h3>
+      <?php if (!empty($message)) : ?>
+         <div class="message">
+            <?php foreach ($message as $msg) : ?>
+               <p><?= $msg ?></p>
+            <?php endforeach; ?>
+         </div>
+      <?php endif; ?>
 
-      <div class="display-orders">
-      <?php
-         $grand_total = 0;
-         $cart_items[] = '';
-         $select_cart = $conn->prepare("SELECT * FROM `cart` WHERE user_id = ?");
-         $select_cart->execute([$user_id]);
-         if($select_cart->rowCount() > 0){
-            while($fetch_cart = $select_cart->fetch(PDO::FETCH_ASSOC)){
-               $cart_items[] = $fetch_cart['name'].' ('.$fetch_cart['price'].' x '. $fetch_cart['quantity'].') - ';
-               $total_products = implode($cart_items);
-               $all_links[] = $fetch_cart['link'].', ';
-               $link = implode($all_links);
-               $grand_total += ($fetch_cart['price'] * $fetch_cart['quantity']);
-      ?>
-         <p> <?= $fetch_cart['name']; ?> <span>(<?= '$'.$fetch_cart['price'].'/- x '. $fetch_cart['quantity']; ?>)</span> </p>
-      <?php
+      <form action="" method="POST">
+
+         <h3>Your Orders</h3>
+
+         <div class="display-orders">
+            <?php
+            $grand_total = 0;
+            $cart_items = [];
+            $select_cart = $conn->prepare("SELECT * FROM `cart` WHERE user_id = ?");
+            $select_cart->execute([$user_id]);
+            if ($select_cart->rowCount() > 0) {
+               while ($fetch_cart = $select_cart->fetch(PDO::FETCH_ASSOC)) {
+                  $cart_items[] = $fetch_cart['name'] . ' (' . $fetch_cart['price'] . ' x ' . $fetch_cart['quantity'] . ') - ';
+                  $total_products = implode($cart_items);
+                  $all_links[] = $fetch_cart['link'] . ', ';
+                  $link = implode($all_links);
+                  $grand_total += ($fetch_cart['price'] * $fetch_cart['quantity']);
+            ?>
+                  <p><?= $fetch_cart['name'] ?> <span>(<?= '$' . $fetch_cart['price'] . '/- x ' . $fetch_cart['quantity'] ?>)</span></p>
+            <?php
+               }
+            } else {
+               echo '<p class="empty">Your cart is empty!</p>';
             }
-         }else{
-            echo '<p class="empty">your cart is empty!</p>';
-         }
-      ?>
-         <input type="hidden" name="total_products" value="<?= $total_products; ?>">
-         <input type="hidden" name="total_price" value="<?= $grand_total; ?>" value="">
-         <input type="hidden" name="link" value="<?= $link; ?>" value="">         
-         <div class="grand-total">grand total : <span>$<?= $grand_total; ?>/-</span></div>
-      </div>
-
-      <h3>place your orders</h3>
-
-      <div class="flex">
-         <div class="inputBox">
-            <span>your name :</span>
-            <input type="text" name="name" placeholder="enter your name" class="box" maxlength="20" required>
+            ?>
+            <input type="hidden" name="total_products" value="<?= $total_products ?>">
+            <input type="hidden" name="total_price" value="<?= $grand_total ?>">
+            <input type="hidden" name="link" value="<?= $link ?>">
+            <div class="grand-total">Grand Total: <span>$<?= $grand_total ?>/-</span></div>
          </div>
-         <div class="inputBox">
-            <span>your number :</span>
-            <input type="number" name="number" placeholder="enter your number" class="box" min="0" max="9999999999" onkeypress="if(this.value.length == 10) return false;" required>
+
+         <h3>Place Your Orders</h3>
+
+         <div class="flex">
+            <div class="inputBox">
+               <span>Your Name:</span>
+               <input type="text" name="name" placeholder="Enter your name" class="box" maxlength="20" required>
+            </div>
+            <div class="inputBox">
+               <span>Your Number:</span>
+               <input type="number" name="number" placeholder="Enter your number" class="box" min="0" max="9999999999" onkeypress="if(this.value.length == 10) return false;" required>
+            </div>
+            <div class="inputBox">
+               <span>Your Email:</span>
+               <input type="email" name="email" placeholder="Enter your email" class="box" maxlength="50" required>
+            </div>
+            <div class="inputBox">
+               <span>Payment Method:</span>
+               <select name="method" class="box" required>
+                  <option value="cash on delivery">Cash on Delivery</option>
+                  <option value="credit card">Credit Card</option>
+                  <option value="paytm">Paytm</option>
+                  <option value="paypal">PayPal</option>
+               </select>
+            </div>
          </div>
-         <div class="inputBox">
-            <span>your email :</span>
-            <input type="email" name="email" placeholder="enter your email" class="box" maxlength="50" required>
-         </div>
-         <div class="inputBox">
-            <span>payment method :</span>
-            <select name="method" class="box" required>
-               <option value="cash on delivery">cash on delivery</option>
-               <option value="paytm">khalti</option>
-            </select>
-         </div>
-         <div class="inputBox">
-            <span>address line 01 :</span>
-            <input type="text" name="flat" placeholder="e.g. flat number" class="box" maxlength="50" required>
-         </div>
-         <div class="inputBox">
-            <span>address line 02 :</span>
-            <input type="text" name="street" placeholder="e.g. street name" class="box" maxlength="50" required>
-         </div>
-         <div class="inputBox">
-            <span>city :</span>
-            <input type="text" name="city" placeholder="e.g. mumbai" class="box" maxlength="50" required>
-         </div>
-         <div class="inputBox">
-            <span>state :</span>
-            <input type="text" name="state" placeholder="e.g. maharashtra" class="box" maxlength="50" required>
-         </div>
-         <div class="inputBox">
-            <span>country :</span>
-            <input type="text" name="country" placeholder="e.g. India" class="box" maxlength="50" required>
-         </div>
-         <div class="inputBox">
-            <span>pin code :</span>
-            <input type="number" min="0" name="pin_code" placeholder="e.g. 123456" min="0" max="999999" onkeypress="if(this.value.length == 6) return false;" class="box" required>
-         </div>
-      </div>
 
-      <input type="submit" name="order" class="btn <?= ($grand_total > 1)?'':'disabled'; ?>" value="place order">
+         <input type="submit" name="order" class="btn <?= ($grand_total > 1) ? '' : 'disabled' ?>" value="Place Order">
 
-   </form>
+      </form>
 
-</section>
+   </section>
 
+   <?php include 'components/footer.php'; ?>
 
-
-
-
-
-
-
-
-
-
-
-
-<?php include 'components/footer.php'; ?>
-
-<script src="js/script.js"></script>
+   <script src="js/script.js"></script>
 
 </body>
+
 </html>
